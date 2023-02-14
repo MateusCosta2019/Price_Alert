614c32821c5c3833bd82863da3052fa7817ccd6d
MateusCosta2019 <mateusjrcosta@hotmail.com>
2023-02-14 15:12:47 -0300

    commit

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def sendtobucket():
     upload.upload_to_s3()
     upload.drop_files()
 
-def executorteste():
+def executorMagazine():
     url = 'https://www.magazineluiza.com.br/celulares-e-smartphones/l/te/'
     proj_path = os.path.dirname(__file__)
     filename = f'DataSmartPhonesMagazineLuiza{datetime.today().strftime("%Y%m%d")}.csv'
@@ -51,7 +51,40 @@ def executorteste():
             break
     
     scrapper.extract_csv()
+
+def executorML():
+    url = 'https://lista.mercadolivre.com.br/celulares-smartphones#deal_print_id=c0e995d0-a98d-11ed-b11b-a59bf15bac8d&c_id=carousel&c_element_order=1&c_campaign=BOLOTA_CELULARES-E-SMARTPHONES&c_uid=c0e995d0-a98d-11ed-b11b-a59bf15bac8d'    
+    proj_path = os.path.dirname(__file__)
+    filename = f'DataSmartPhonesML{datetime.today().strftime("%Y%m%d")}.csv'
+    direxport = os.path.join(proj_path, 'datasets', filename)
+    variables = dict({
+        'Cards': ['div', 'ui-search-result__wrapper shops__result-wrapper'],
+        'Item': ['h2','ui-search-item__title shops__item-title'],
+        'Original_Price': ['s', 'price-tag ui-search-price__part ui-search-price__original-value shops__price-part price-tag__disabled'],
+        'Price': ['span', 'price-tag-fraction'],
+        'Installment': ['span', 'ui-search-item__group__element shops__items-group-details ui-search-installments ui-search-color--LIGHT_GREEN'],
+        'Reviews': ['span', 'ui-search-reviews__amount'],
+        })
+
+    scrapper = ScrapeData(logger=LOGGER_OBJ, url=url, tag_cookies='', tag_btn_pag='.andes-pagination__button--next .andes-pagination__link', 
+                        path=direxport, separtor='|', variables=variables)
+    
+    rep = 0
+
+    while rep < 16:
+        rep += 1
+        try:
+            scrapper.scraping()
+            LOGGER_OBJ.info("--> Crawler performed successfully.")
+
+            scrapper.next_page()
+        except Exception as e:
+            print(e)
+            break
+    
+    scrapper.extract_csv()
     
 if __name__=='__main__':
-    executorteste()
-    # sendtobucket()
+    executorMagazine()
+    executorML()
+    sendtobucket()
